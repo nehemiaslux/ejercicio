a41d1f73b882abd697f2aa02793537a1f5a9f506
nehemiaslux <39443377+nehemiaslux@users.noreply.github.com>
2018-05-19 13:12:58 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>PHP </title>
+	</head>
+	<body>
+		<p>
+		
+
+		<?php
+		//Declaracion de variables
+		$Pcantidad=17;
+		$Scantidad=3;
+		$Total=$Pcantidad+$Scantidad;
+		if($Total >=18){
+			echo "Mayor a 18";
+			}else{
+				echo "Menor a 18";
+			}
+
+
+
+
+
+
+
+
+
+
+
+?>
+</p>
+</body>
+</html
